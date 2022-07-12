18fcc36c4d46cded3ab67385a28e3b36072fda5b
Oluseun Dami <oluseundamilola@gmail.com>
2022-07-12 15:10:55 +0100

    shopie commit

---
+++ src/data.js
@@ -0,0 +1,97 @@
+export const sliderItems = [
+    {
+        id: 1,
+        img: '../../assets/images/slide1.png',
+        title: "AUTUMN COLLECTION",
+        desc: "DON'T COMPROMISE ON STYLE! GET FLAT 30% OFF FOR NEW ARRIVALS.",
+        bg: "#fff491",
+      },
+
+      {
+        id: 2,
+        img: "../../assets/images/slide2.png",
+        title: "LOUNGEWEAR LOVE",
+        desc: "DON'T COMPROMISE ON STYLE! GET FLAT 30% OFF FOR NEW ARRIVALS.",
+        bg: "fcf1ed",
+      },
+
+      {
+        id: 3,
+        img: "../../assets/images/slide3.png",
+        title: "SUMMER SALE",
+        desc: "DON'T COMPROMISE ON STYLE! GET FLAT 30% OFF FOR NEW ARRIVALS.",
+        bg: "fbf0f4",
+      },
+
+];
+
+export const categories = [
+  {
+    id: 1,
+    img: "../../assets/images/men.jpg",
+    title: "JACKETS",
+  },
+  {
+    id: 2,
+    img:"../../assets/images/women.jpg",
+    title: "SUMMER",
+  },
+  {
+    id: 3,
+    img:"../../assets/images/hat.jpg",
+    title: "CASUAL",
+  },
+];
+
+export const popularProducts = [
+  {
+    id:1,
+    img: "../../assets/images/p1.png",
+  },
+  {
+    id:2,
+    img: "../../assets/images/p2.png",
+  },
+  {
+    id:3,
+    img: "../../assets/images/p3.png",
+  },
+  {
+    id:4,
+    img: "../../assets/images/p8.png",
+  },
+  {
+    id:5,
+    img: "../../assets/images/p4.jpg",
+  },
+  {
+    id:6,
+    img: "../../assets/images/p5.png",
+  },
+  {
+    id:7,
+    img: "../../assets/images/p6.png",
+  },
+  {
+    id:8,
+    img: "../../assets/images/p7.png",
+  },
+];
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
